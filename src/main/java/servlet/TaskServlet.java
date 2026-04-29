2530e489ca407d01bc0ca884f126fef581f22e14
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-04-29 15:07:58 +0300

    Add Thymeleaf templates

--- src/main/java/servlet/TaskServlet.java
+++ src/main/java/servlet/TaskServlet.java
@@ -5,17 +5,44 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 import service.TaskService;
 
 import java.io.IOException;
 
 @WebServlet("/tasks")
 public class TaskServlet extends HttpServlet {
-    TaskService taskService = new TaskService();
+    private TaskService taskService = new TaskService();
+    private TemplateEngine templateEngine;
+    private JakartaServletWebApplication application;
+
+    @Override
+    public void init() throws ServletException {
+        this.application = JakartaServletWebApplication.buildApplication(getServletContext());
+        WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
-        resp.getWriter().write(taskService.findAll().toString());
+
+        IWebExchange  exchange = this.application.buildExchange(req, resp);
+        WebContext context = new WebContext(exchange, exchange.getLocale());
+
+        context.setVariable("tasks", taskService.findAll());
+        resp.setContentType("text/html; charset=utf-8");
+        templateEngine.process("tasks", context, resp.getWriter());
     }
 }
